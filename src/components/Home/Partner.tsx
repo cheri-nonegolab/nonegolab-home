d1acbe96c69a40a8deea7fd9add958aab0eb5ec8
cheri-nonegolab <cheri@nonegolab.com>
2022-08-26 12:00:51 +0900

    feat ::: home-partner 영역 잡기

--- src/components/Home/Partner.tsx
+++ src/components/Home/Partner.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Wrapper } from "src/layouts";
+import { Text } from "../common";
 
 export default function Partner() {
     return (
-        <section className="h-[calc(100%+15px)] w-[100vw] bg-gray -mt-[15px]">
-            <Wrapper className="flex justify-between items-center"></Wrapper>
+        <section className="h-[calc(50%)] w-[100vw] bg-white py-[30px]">
+            <Wrapper>
+                <Text type="h3" className="mb-[30px]">
+                    Partner
+                </Text>
+                <div className="h-[calc(100%-90px)] bg-gray"></div>
+            </Wrapper>
         </section>
     );
 }
